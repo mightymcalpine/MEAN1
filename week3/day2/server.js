f387b41c39979125787b3a6b96c1424155630262
dojo-json <jfranz@codingdojo.com>
2017-06-21 16:37:44 -0700

    express middleware

--- week3/day2/server.js
+++ week3/day2/server.js
@@ -9,13 +9,63 @@ app.use(parser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('views'));
 
-app.get('/', function(req, res){
+app.use(function(request, response, next) {
+  console.log('in our middleware');
+
+  next();
+});
+
+// app.use(function(request, response, next) {
+//   // console.log(`Request received on ${ new Date() }`);
+//   request.timeStamp = new Date();
+//   next()
+// });
+
+function dateRequested({ getOnly = true, errorIf = false, body = false, ignore = false } = {}) {
+  console.log(getOnly, errorIf, body, ignore);
+
+  return function(request, response, next) {
+    if(ignore){
+      return next();
+    }
+    if (getOnly) {
+      if (request.method !== 'GET') {
+        let error = null;
+        if (errorIf) {
+          error = new Error('This Method is not supported!');
+        }
+
+        return next(error);
+      }
+    }
+    if (body) {
+      request.body.timeStamp = new Date();
+    } else {
+      request.timeStamp = new Date();
+    }
+
+    // handle body
+
+    next();
+  }
+}
+
+// app.use(dateRequested({ body: true, getOnly: false }));
+
+
+
+app.get('/', dateRequested(), function(req, res){
+  console.log(req.timeStamp);
   console.log('arrive at this point!!!');
   res.render('index');
 });
 
-app.post('/', function(req, res){
+
+
+app.post('/', dateRequested({ getOnly: false, body: true }), function(req, res){
   console.log(req.body);
+  console.log(req.timeStamp);
+  //console.log(`Request received on ${ new Date() }`);
 
   const names = ['Jason', 'Marty', 'Selvi'];
 
@@ -25,3 +75,21 @@ app.post('/', function(req, res){
 });
 
 app.listen(port, () => console.log(`listening to server on ${port}`));
+
+
+
+
+/**
+* Create middleware that reports information about the incoming http request
+* Certain elements will be objects(body), display the key value pairs
+* Items to report iff they have value, use colors (an external module):
+*                 method
+*                 hostname
+*                 ip
+*                 body
+*                 cookies
+*                 parameters
+*                 protocol
+*                 route
+*                 url
+*/
